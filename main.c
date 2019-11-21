74f0ebd89dacaa001b778ed252fd4fcc9f47992d
usmanniaz12 <58037642+usmanniaz12@users.noreply.github.com>
2019-11-21 20:24:18 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
-    printf("Hello world!\n");
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
     return 0;
 }
